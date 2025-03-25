5ac7e093053b9a510e50dcdbbc9a0b2fbc61e18d
Emir <emireugenio3@gmail.com>
2025-03-25 14:23:33 -0300

    Adicionando URLs

--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+
+class Criar(View):
+    ...
+
+
+class Atualizar(View):
+    ...
+
+
+class Login(View):
+    ...
+
+
+class Logout(View):
+    ...
